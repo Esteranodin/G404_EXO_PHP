38f6abdfc061011eebdc4c2771fe2cc237091f4f
esteranodin <esteranodin@hotmail.fr>
2024-12-04 16:13:35 +0100

    DONE: partie5

--- Partie 5/index.php
+++ Partie 5/index.php
@@ -1,31 +1,50 @@
-<!-- ---------- EXO 1 ---------- -->
+<!-- ---------- EXO 1---------- -->
+<!-- <?php
+$months = array("janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre");
+
+//  <!-- ---------- EXO 2 ---------- -->
+// echo $months[2];
+
+//  <!-- ---------- EXO 3 ---------- -->
+
+// echo $months[5];
+
+//  <!-- ---------- EXO 4 ---------- -->
+
+//  $months[7] = "août";
+//  echo $months[7];
+
+?> -->
+
+<!-- ---------- EXO 5 ---------- -->
 <?php
- 
- ?> 
- <!-- ---------- EXO 2 ---------- -->
-  <?php
- ?> 
- <!-- ---------- EXO 3 ---------- -->
-  <?php
- ?> 
- <!-- ---------- EXO 4 ---------- -->
-  <?php
- ?> 
- <!-- ---------- EXO 5 ---------- -->
-  <?php
- ?> 
- <!-- ---------- EXO 6 ---------- -->
-  <?php
- ?> 
- <!-- ---------- EXO 7 ---------- -->
-  <?php
- ?> 
- <!-- ---------- EXO 8 ---------- -->
-  <?php
- ?> 
- <!-- ---------- EXO 9 ---------- -->
-  <?php
- ?> 
- <!-- ---------- EXO 10 ---------- -->
-  <?php
- ?> 
+$departements = array (2 =>"Aisne", 59 =>"Nord", 60 =>"Oise", 62 =>"Pas-de-Calais", 80 =>"Somme" );
+
+// <!-- ---------- EXO 6 ---------- -->
+
+// echo $departements[59];
+
+// <!-- ---------- EXO 7 ---------- -->
+
+// $departements[51] = "Marne";
+// echo $departements[51];
+
+
+// <!-- ---------- EXO 8 ---------- -->
+
+// foreach ($months as $value) {
+//    echo $value . "<br>";
+// }
+
+// <!-- ---------- EXO 9 ---------- -->
+
+// foreach ($departements as $value) {
+//     echo $value . "<br>";
+// }
+
+// <!-- ---------- EXO 10 ---------- -->
+
+// foreach ($departements as $key => $value) {
+//     echo "Le département {$value} a le numéro {$key} <br>";
+// }
+?>
